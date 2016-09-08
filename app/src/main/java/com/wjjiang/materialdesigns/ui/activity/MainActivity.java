80044261212c8efa2c17c3a1d66c550cf055cfc7
Jiangwenjin <jiang@thankyo.net>
2016-09-08 14:53:53 +0500

    home page

--- app/src/main/java/com/wjjiang/materialdesigns/ui/activity/MainActivity.java
+++ app/src/main/java/com/wjjiang/materialdesigns/ui/activity/MainActivity.java
@@ -1,11 +1,16 @@
 package com.wjjiang.materialdesigns.ui.activity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
@@ -25,6 +30,14 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     NavigationView mNavView;
     @BindView(R.id.fab)
     FloatingActionButton mFab;
+    @BindView(R.id.collapsing_toolbar_layout)
+    CollapsingToolbarLayout mCollapsingToolbarLayout;
+    @BindView(R.id.app_bar)
+    AppBarLayout mAppBarLayout;
+
+    private ActionBarDrawerToggle toggle;
+    private int scrollRange = -1;
+    private boolean isShow = false;
 
     @Override
     protected int getLayoutResId() {
@@ -35,9 +48,21 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(0);
+        }
+
         setSupportActionBar(mToolbar);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, mDrawer, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mDrawer.post(new Runnable() {
+            @Override
+            public void run() {
+                toggle.syncState();
+            }
+        });
+        toggle = new ActionBarDrawerToggle(this, mDrawer, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        toggle.setDrawerIndicatorEnabled(true);
         mDrawer.addDrawerListener(toggle);
         toggle.syncState();
 
@@ -49,6 +74,11 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
                         .setAction("Action", null).show();
             }
         });
+
+        mCollapsingToolbarLayout.setTitle("MD");
+        mCollapsingToolbarLayout.setExpandedTitleColor(ViewCompat.MEASURED_STATE_MASK);
+        mCollapsingToolbarLayout.setCollapsedTitleTextColor(-1);
+        mAppBarLayout.addOnOffsetChangedListener(new CustomOnOffsetChangedListener());
     }
 
     @Override
@@ -82,6 +112,29 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
             mDrawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        toggle.onConfigurationChanged(newConfig);
+    }
+
+    class CustomOnOffsetChangedListener implements AppBarLayout.OnOffsetChangedListener {
+        @Override
+        public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+            if (scrollRange == -1) {
+                scrollRange = appBarLayout.getTotalScrollRange();
+            }
+
+            if (scrollRange + verticalOffset == 0) {
+                mCollapsingToolbarLayout.setTitle("Material Designs");
+                isShow = true;
+            } else if (isShow) {
+                mCollapsingToolbarLayout.setTitle("MD");
+                isShow = false;
+            }
         }
     }
 }
